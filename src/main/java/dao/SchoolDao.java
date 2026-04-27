03d0e981d788cdaf3d487c264ef7473460ca86f4
tyaotyao1117-wq <tyaotyao1117@gmail.com>
2026-04-27 14:32:54 +0900

    プロジェクト新規作成

--- src/main/java/dao/SchoolDao.java
+++ src/main/java/dao/SchoolDao.java
@@ -8,46 +8,60 @@ import java.sql.SQLException;
 import bean.School;
 
 public class SchoolDao extends Dao {
-	public School get(String cd)throws Exception{
+	/**
+	 * getメソッド 学校コードを指定して学校インスタンスを１件取得する
+	 *
+	 * @param cd:String
+	 *            学校コード
+	 * @return 学校クラスのインスタンス 存在しない場合はnull
+	 * @throws Exception
+	 */
+	public School get(String cd) throws Exception {
+		// 学校インスタンスを初期化
 		School school = new School();
-		Connection connection= getConnection();
+		// データベースへのコネクションを確率
+		Connection connection = getConnection();
+		// プリペアードステートメント
 		PreparedStatement statement = null;
 
 		try {
-			statement = connection.prepareStatement("select * from school where cd=?");
-			
+			// プリペアードステートメントにSQL文をセット
+			statement = connection.prepareStatement("select * from school where cd = ?");
+			// プリペアードステートメントに学校コードをバインド
 			statement.setString(1, cd);
-			
+			// プリペアードステートメントを実行
 			ResultSet rSet = statement.executeQuery();
-			
-			if(rSet.next()) {
+
+			if (rSet.next()) {
+				// リザルトセットが存在する場合
+				// 学校インスタンスに学校コードと学校名をセット
 				school.setCd(rSet.getString("cd"));
 				school.setName(rSet.getString("name"));
-			}else {
-				
+			} else {
+				// 存在しない場合
+				// 学校インスタンスにnullをセット
 				school = null;
 			}
-		}catch (Exception e) {
+		} catch (Exception e) {
 			throw e;
-		
-		}finally {
-			
-			if(statement !=null) {
+		} finally {
+			// プリペアードステートメントを閉じる
+			if (statement != null) {
 				try {
 					statement.close();
-				}catch(SQLException sqle) {
+				} catch (SQLException sqle) {
 					throw sqle;
 				}
 			}
-			if(connection !=null) {
+			// コネクションを閉じる
+			if (connection != null) {
 				try {
 					connection.close();
-				}catch (SQLException sqle) {
+				} catch (SQLException sqle) {
 					throw sqle;
 				}
 			}
 		}
 		return school;
 	}
-
 }
